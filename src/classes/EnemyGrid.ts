4dbffd5e68c7d55e8aa1040af60684d7aa162b74
Noopel <30575718+Noopel@users.noreply.github.com>
2024-09-26 09:06:17 +0200

    attempt 2

--- src/classes/EnemyGrid.ts
+++ src/classes/EnemyGrid.ts
@@ -1,6 +1,6 @@
 import CustomElement from "./CustomClasses/CustomElement"
 
-const imagePath = "../public/assets/img/"
+const imagePath = "../assets/img/"
 
 class EnemyGrid {
     constructor(enemyList: EnemyData[], parent: HTMLElement | Element) {
--- src/main.ts
+++ src/main.ts
@@ -1,85 +1,80 @@
-import './style.scss'
+import "./style.scss";
 
-import 'pixi.js/math-extras';
+import "pixi.js/math-extras";
 
-import characterData from "../public/assets/json/characterData.json"
-import CharacterCard from './classes/CharacterCard'
-import { Application, Assets } from 'pixi.js';
-import Game from './classes/Game/Game';
-import charData from "../public/assets/json/characterData.json"
-import enemyData from "../public/assets/json/enemyData.json"
-import GameStatistics from './classes/GameStatistics';
-import EnemyGrid from './classes/EnemyGrid';
+import characterData from "../src/assets/json/characterData.json";
+import CharacterCard from "./classes/CharacterCard";
+import { Application, Assets } from "pixi.js";
+import Game from "./classes/Game/Game";
+import charData from "../src/assets/json/characterData.json";
+import enemyData from "../src/assets/json/enemyData.json";
+import GameStatistics from "./classes/GameStatistics";
+import EnemyGrid from "./classes/EnemyGrid";
 
 declare module globalThis {
- var __PIXI_APP__: Application;
+  var __PIXI_APP__: Application;
 }
 
 const app = new Application();
 globalThis.__PIXI_APP__ = app;
 
-let assetBundle: {[key: string]: string} = {
-  playButton: "../public/assets/img/playButton.png",
-  bulletSprite: "../public/assets/img/bullet.png"
-
-}
+let assetBundle: { [key: string]: string } = {
+  playButton: "../src/assets/img/playButton.png",
+  bulletSprite: "../src/assets/img/bullet.png",
+};
 
 /** Create bundle data for each character */
 charData.characterList.forEach((charData) => {
-  assetBundle[charData.name + "_sprite"] = `../public/assets/img/${charData.characterSprite}`
-  assetBundle[charData.name + "_icon"] = `../public/assets/img/${charData.icon}`
+  assetBundle[charData.name + "_sprite"] = `../src/assets/img/${charData.characterSprite}`;
+  assetBundle[charData.name + "_icon"] = `../src/assets/img/${charData.icon}`;
 });
 
 /** Create bundle data for each enemy */
-enemyData.enemyList.forEach((enemData)=>{
-  assetBundle[enemData.name + "_sprite"] = `../public/assets/img/${enemData.spriteName}`
-  assetBundle[enemData.name + "_icon"] = `../public/assets/img/${enemData.icon}`
-})
+enemyData.enemyList.forEach((enemData) => {
+  assetBundle[enemData.name + "_sprite"] = `../src/assets/img/${enemData.spriteName}`;
+  assetBundle[enemData.name + "_icon"] = `../src/assets/img/${enemData.icon}`;
+});
 
 Assets.addBundle("assets", assetBundle);
 
-(async()=>{
-
-
+(async () => {
   /** Setting up character cards */
   let cardCon = document.querySelector("#characterContainer") as HTMLElement;
-  characterData.characterList.forEach((charData)=>{
-    new CharacterCard(charData, cardCon)
-  })
+  characterData.characterList.forEach((charData) => {
+    new CharacterCard(charData, cardCon);
+  });
 
   /** Setting up enemy grid */
   let gridCon = document.querySelector("#enemyContainer") as HTMLElement;
-  new EnemyGrid(enemyData.enemyList, gridCon)
+  new EnemyGrid(enemyData.enemyList, gridCon);
 
   /** Creates game canvas */
   let gameContainer = document.querySelector("#gameContainer") as HTMLElement;
 
-  let width = gameContainer.clientWidth
-  let aspectRatio = 16/9
+  let width = gameContainer.clientWidth;
+  let aspectRatio = 16 / 9;
 
   /** Initialize app and add window resizer */
-  await app.init({width: Number(width), height: Number(width)/aspectRatio, manageImports: true})
-  window.onresize = function(_event) {
-    app.renderer.resize(Number(gameContainer.clientWidth), Number(gameContainer.clientWidth)/aspectRatio)    
-  }
+  await app.init({ width: Number(width), height: Number(width) / aspectRatio, manageImports: true });
+  window.onresize = function (_event) {
+    app.renderer.resize(Number(gameContainer.clientWidth), Number(gameContainer.clientWidth) / aspectRatio);
+  };
 
-  gameContainer.appendChild(app.canvas)
-  
+  gameContainer.appendChild(app.canvas);
+
   /** Gets assets from bundle */
-  const assets = await Assets.loadBundle('assets');
+  const assets = await Assets.loadBundle("assets");
 
-  console.log(assets)
+  console.log(assets);
 
   /** Pre-setup for GameStatistics so that it's updateCharts method can be sent to Game as a callback */
-  const gameStatistics = new GameStatistics({characterList: charData.characterList, enemyList: enemyData.enemyList});
+  const gameStatistics = new GameStatistics({ characterList: charData.characterList, enemyList: enemyData.enemyList });
 
-  new Game(app, assets, {characterList: charData.characterList, enemyList: enemyData.enemyList}, (gameSessions: SessionData[])=>{
-    gameStatistics.updateCharts(gameSessions[gameSessions.length-1])
-  })
+  new Game(app, assets, { characterList: charData.characterList, enemyList: enemyData.enemyList }, (gameSessions: SessionData[]) => {
+    gameStatistics.updateCharts(gameSessions[gameSessions.length - 1]);
+  });
 
   /** Parent GameStatistics */
   let main = document.querySelector("main") as HTMLElement;
-  gameStatistics.setParent(main)
-
-
-})()
+  gameStatistics.setParent(main);
+})();
