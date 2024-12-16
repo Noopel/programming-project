8440c6affdec09f7da4e39279c82b96d37e383ed
Noopel <30575718+Noopel@users.noreply.github.com>
2024-12-16 09:38:18 +0100

    Fixed base path for build

---
+++ dist/assets/batchSamplersUniformGroup-BDhSxhjo.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index-9IWxx4qv.js";import{U as o}from"./colorToUniform-CiacwDhX.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
